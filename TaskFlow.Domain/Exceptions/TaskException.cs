218fea0d67cb57ad4754ce9f906b7fc0092a8158
adesduende <sergiolopezlobo1990@gmail.com>
2025-07-20 15:12:10 +0200

    Add custom exceptions to entities

---
+++ TaskFlow.Domain/Exceptions/TaskException.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Domain.Exceptions
+{
+    public static class TaskException
+    {
+        public class EmptyTitleException : Exception
+        {
+            public EmptyTitleException() : base("Title cannot be empty.") { }
+        }
+
+        public class TitleExceedLimitException : Exception
+        {
+            public TitleExceedLimitException(int rateLimit) : base($"Title cannot exceed {rateLimit} characters.") { }
+        }
+
+        public class TitleException : Exception
+        {
+            public TitleException(string title) : base($"Title must be different from the current title: {title}.") { }
+        }
+
+        public class EmptyDescriptionException : Exception
+        {
+            public EmptyDescriptionException() : base("Description cannot be empty.") { }
+        }
+        public class DescriptionExceedLimitException : Exception
+        {
+            public DescriptionExceedLimitException(int rateLimit) : base($"Description cannot exceed {rateLimit} characters.") { }
+        }
+        public class DescriptionException : Exception
+        {
+            public DescriptionException(string description) : base($"Description must be different from the current description: {description}.") { }
+        }
+        public class EmptyPriorityException : Exception
+        {
+            public EmptyPriorityException() : base("Priority cannot be empty.") { }
+        }
+        public class EmptyStatusException : Exception
+        {
+            public EmptyStatusException() : base("Status cannot be empty.") { }
+        }
+    }
+}
---
+++ TaskFlow.Domain/Exceptions/UserException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Domain.Exceptions
+{
+    public static class UserException
+    {
+        public class EmptyNameException : Exception
+        {
+            public EmptyNameException() : base("Name cannot be empty.") { }
+        }
+        public class NameExceedLimitException : Exception
+        {
+            public NameExceedLimitException(int limit) : base($"Name cannot exceed {limit} characters.") { }
+        }
+        public class NameException : Exception
+        {
+            public NameException(string name) : base($"Name must be different from the current name: {name}.") { }
+        }
+        public class EmptyEmailException : Exception
+        {
+            public EmptyEmailException() : base("Email cannot be empty.") { }
+        }
+        public class InvalidEmailFormatException : Exception
+        {
+            public InvalidEmailFormatException(string email) : base($"Invalid email format: {email}.") { }
+        }
+    }
+}
