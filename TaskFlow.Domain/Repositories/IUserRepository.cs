716303c3fedea2c85ecb7b67cfab59eb73b6699b
adesduende <sergiolopezlobo1990@gmail.com>
2025-07-20 12:14:49 +0200

    Add repositories

---
+++ TaskFlow.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        System.Threading.Tasks.Task<IEnumerable<Entities.User>> GetAllUsersAsync();
+        System.Threading.Tasks.Task<Entities.User> GetUserByIdAsync(Guid userId);
+        System.Threading.Tasks.Task<Entities.User> GetUserByEmailAsync(string email);
+        System.Threading.Tasks.Task AddUserAsync(Entities.User user);
+        System.Threading.Tasks.Task UpdateUserAsync(Entities.User user);
+        System.Threading.Tasks.Task DeleteUserAsync(Guid userId);
+        System.Threading.Tasks.Task<bool> UserExistsAsync(Guid userId);
+        System.Threading.Tasks.Task<bool> UserEmailExistsAsync(string email);
+    }
+}
