a9ac919a288dd509157aadc4a4bfaeaf7700e7dc
adesduende <sergiolopezlobo1990@gmail.com>
2025-07-20 18:48:02 +0200

    Add mappers for Task and User

---
+++ TaskFlow.Application/Mappers/TaskMapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Application.Mappers
+{
+    public static class TaskMapper
+    {
+        /// <summary>
+        /// Maps a task entity to a DTO.
+        /// </summary>
+        /// <param name="task">The task entity to map.</param>
+        /// <returns>A DTO representing the task.</returns>
+        public static DTO.TaskDTO ToDto(this Domain.Entities.Task task)
+        {
+            if (task == null) throw new ArgumentNullException(nameof(task));
+            return new DTO.TaskDTO(
+                task.Id.ToString(),
+                task.Title,
+                task.Description,
+                task.Priority.ToString(),
+                task.Status.ToString(),
+                task.CreatedAt,
+                task.TimeLimit.HasValue ? task.TimeLimit : null,
+                task.UserId.HasValue ? task.UserId.Value.ToString() : string.Empty
+            );
+        }
+    }
+}
---
+++ TaskFlow.Application/Mappers/UserMapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Application.Mappers
+{
+    public static class UserMapper
+    {
+        /// <summary>
+        /// Maps a user entity to a DTO.
+        /// </summary>
+        /// <param name="user">The user entity to map.</param>
+        /// <returns>A DTO representing the user.</returns>
+        public static DTO.UserDTO ToDto(this Domain.Entities.User user)
+        {
+            if (user == null) throw new ArgumentNullException(nameof(user));
+            return new DTO.UserDTO(
+                user.Id.ToString(),
+                user.Name,
+                user.Email
+            );
+        }
+    }
+}
