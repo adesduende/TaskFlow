e0e918ce0547f684cecf6875081ac2841b79e77e
adesduende <sergiolopezlobo1990@gmail.com>
2025-07-20 18:47:29 +0200

    Update to return String instead int

--- TaskFlow.Application/DTO/UserDTO.cs
+++ TaskFlow.Application/DTO/UserDTO.cs
@@ -8,11 +8,11 @@ namespace TaskFlow.Application.DTO
 {
     public class UserDTO
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
 
-        public UserDTO(int id, string name, string email)
+        public UserDTO(string id, string name, string email)
         {
             Id = id;
             Name = name;
