9a6239d041e7073f04ae47f17c91ca6e3460cb33
adesduende <sergiolopezlobo1990@gmail.com>
2025-07-21 21:15:53 +0200

    Fix Method to AddApplication

--- src/TaskFlow.Application/DependencyInjection.cs
+++ src/TaskFlow.Application/DependencyInjection.cs
@@ -4,7 +4,7 @@ namespace TaskFlow.Application
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             return services;
         }
