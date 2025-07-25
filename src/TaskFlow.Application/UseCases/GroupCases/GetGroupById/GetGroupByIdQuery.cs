df6f00a4fa86422eddde477f7abbbea1307a15bd
Sergio <sergiolopezlobo@proton.me>
2025-07-25 19:59:28 +0200

    Add Get group by id use case

---
+++ src/TaskFlow.Application/UseCases/GroupCases/GetGroupById/GetGroupByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskFlow.Application.DTO;
+using TaskFlow.Application.Interfaces;
+
+namespace TaskFlow.Application.UseCases.GroupCases.GetGroupById
+{
+    public record GetGroupByIdQuery(Guid id) : IRequest<GroupDTO>;
+}
