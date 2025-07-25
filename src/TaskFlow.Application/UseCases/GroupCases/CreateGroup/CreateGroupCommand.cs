45923b84bf792070cbf176ee5daf8073c3754e80
Sergio <sergiolopezlobo@proton.me>
2025-07-25 19:41:25 +0200

    Add create group use case

---
+++ src/TaskFlow.Application/UseCases/GroupCases/CreateGroup/CreateGroupCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskFlow.Application.Interfaces;
+
+namespace TaskFlow.Application.UseCases.GroupCases.CreateGroup
+{
+    public record CreateGroupCommand(
+        string name,
+        string description,
+        List<Guid>? users,
+        List<Guid>? tasks
+        ) : IRequest<Guid>;
+}
