b235a8c6d5a58fee467228f98be5b55af00d863a
Sergio <sergiolopezlobo@proton.me>
2025-07-26 12:55:17 +0200

    Add global usings

---
+++ src/TaskFlow.WebApi/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using TaskFlow.Application.UseCases.TaskCases.GetAllTasks;
+global using TaskFlow.Application.UseCases.TaskCases.CreateTask;
+global using TaskFlow.Application.UseCases.TaskCases.DeleteTask;
+global using TaskFlow.Application.UseCases.TaskCases.ChangeTaskStatus;
+global using TaskFlow.Application.UseCases.TaskCases.AssignTaskToUser;
+global using TaskFlow.Application.UseCases.TaskCases.GetTaskById;
