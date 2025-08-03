ca29148b2fda0ed0e7fcdb2f126240cc2697c5a3
Sergio <sergiolopezlobo@proton.me>
2025-08-03 14:03:28 +0200

    Update Comand an Handler to return LoginDTO

--- src/TaskFlow.Application/UseCases/UserCases/Login/LoginCommand.cs
+++ src/TaskFlow.Application/UseCases/UserCases/Login/LoginCommand.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskFlow.Application.DTO;
 using TaskFlow.Application.Interfaces;
 
 namespace TaskFlow.Application.UseCases.UserCases.Login
 {
-    public record LoginCommand (string email, string password): IRequest<string>;    
+    public record LoginCommand (string email, string password): IRequest<LoginDTO>;    
 }
